2e1b4e39df4a01646eaa51649ed8a36fd961efd6
Henrik Joreteg <hjoreteg@gmail.com>
2013-03-19 01:57:04 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+#inline emoji
+
+emoji as inlineable data-uris in JSON.
+
+
+## What is this?
+
+Not entirely sure yet. Will probably turn this into a little script for adding emoji to html.
+
+## Why not just use https://github.com/hassankhan/emojify.js?
+
+I want it all in as super simple text processing (without the CSS and DOM stuff) and I want to be able to do the text parsing and then DOMify the result.
+
+## Is it ready to use?
+
+no. But hey... if you want some nice JSON files with emoji data uris... then check the "output" folder. 
---
+++ scraper.js
@@ -0,0 +1,31 @@
+// quick/dirty script to scrape the emoji data and write them to JSON files for consumption elsewhere.
+// props to hassankhan for doing the work of converting these to data uris.
+var request = require('request'),
+    async = require('async'),
+    fs = require('fs');
+
+var urls = [
+    'https://raw.github.com/hassankhan/emojify.js/master/less/emojify.emoticons.less',
+    'https://raw.github.com/hassankhan/emojify.js/master/less/emojify.nature.less',
+    'https://raw.github.com/hassankhan/emojify.js/master/less/emojify.objects.less',
+    'https://raw.github.com/hassankhan/emojify.js/master/less/emojify.people.less',
+    'https://raw.github.com/hassankhan/emojify.js/master/less/emojify.places.less',
+    'https://raw.github.com/hassankhan/emojify.js/master/less/emojify.symbols.less'
+];
+
+async.each(urls, function (url, loopCb) {
+    var results = {};
+    request.get(url, function (err, res, body) {
+        var split = body.split('\n');
+        split.forEach(function (text) {
+            if (!text) return;
+            var text = text.slice(9),
+                name = text.match(/^\S+/)[0];
+
+            text = text.match(/url\((\S+)\)/)[0];
+            results[name] = text.slice(4, -1);
+        });
+        fs.writeFileSync('output/' + url.slice(65, -5) + '.json', JSON.stringify(results, null, 2));
+        loopCb();
+    });
+});
