2ea1be2950ae7a04e1cdee5fe05b8b3c33ab3660
Henrik Joreteg <hjoreteg@gmail.com>
2013-03-31 00:18:51 -0700

    Cleaning up and adding a couple of missing images

--- readme.md
+++ readme.md
@@ -1,20 +1,43 @@
-#emoji.js
+#emoji-images.js
 
 Because sometimes you want to send a :heart: to someone.
 
+
 ## What is this?
 
-Not entirely sure yet. Takes text, replaces all the texts here: http://www.emoji-cheat-sheet.com with an `<img>` tag with the image.
+Takes text, replaces all the texts here: http://www.emoji-cheat-sheet.com with an `<img>` tag containing the image.
+
+The images are all in the [](pngs) folder. Host those somewhere, pass the URL to the function. See below:
+
+
+## How to use it
+
+```js
+
+var string = 'a string containing emoji strings like :heart: and :trollface:';
+
+// the emoji function takes:
+// 1. The input string
+// 2. The url where the png files live
+// 3. (optionally) a size to specify as the height of the <img> tag.
+var emojified = emoji(string, 'http://url.com/where-you-put-the-pngs-folder/', 30); 
+
+```
+
+It's bundled to work as a CommonJS module, AMD module or expose itself as a global as a fallback.
+
 
 ## Why not just use https://github.com/hassankhan/emojify.js?
 
-I want it all in as super simple text processing (without the CSS and DOM stuff).
+I want it all in a super simple text processing (without the CSS and DOM stuff).
 
 Also, that solution embeds pngs as data-uri's in css documents. I don't want to do that because then I have to send all that image data to the browser even if they never get used.
 
+
 ## Is it ready to use?
 
-Um... works for me™
+Um... works for me™  (and in use on andbang.com)
+
 
 ## License
 
--- template.js
+++ template.js
@@ -1,19 +1,31 @@
-var emojis = {{data}},
-    test = /\:[a-z0-9_\-\+]+\:/g;
+// universal module definition: https://github.com/umdjs/umd/blob/master/returnExports.js#L41
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
+    var emojis = {{data}},
+        test = /\:[a-z0-9_\-\+]+\:/g;
 
-function template(name) {
-    var name = String(name).slice(1, -1);
-    return ['<img class="emoji" title=":', name, ':" alt="', name, '" src="https://andbang-emoji.s3.amazonaws.com/', encodeURIComponent(name), '.png"/>'].join('');
-}
+    function emoji(someString, url, size) {
+        return someString.replace(test, function (match) {
+            if (emojis.indexOf(match) !== -1) {
+                var name = String(match).slice(1, -1);
+                return '<img class="emoji" title=":' + name + ':" alt="' + name + '" src="' + url + '/' + encodeURIComponent(name) + '.png"' + (size ? (' height="' + size + '"') : '') + ' />';
+            } else {
+                return match;
+            }
+        });
+    };
 
-function emoji(someString) {
-    return someString.replace(test, function (match) {
-        if (emojis.indexOf(match) !== -1) {
-            return template(match);
-        } else {
-            return match;
-        }
-    });
-};
-
-module.exports = emoji;
+    return emoji;
+}));
